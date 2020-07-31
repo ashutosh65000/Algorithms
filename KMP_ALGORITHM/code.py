baf32036ffcab19a12c53129f7455340d1b0667e
Ashutosh Srivastava <20560757+Ashutosh-Srivastava@users.noreply.github.com>
2020-07-31 14:57:42 +0530

    add author signature

--- KMP_ALGORITHM/code.py
+++ KMP_ALGORITHM/code.py
@@ -1,3 +1,11 @@
+'''
+@author: Ashutosh Srivastava
+@version: 1.0.0
+@email: ashutosh65000@outlook.com
+@copyright = Copyright 2020, The Algorithm Project
+Python3 solution
+'''
+
 string="AABAACAADAABAABA"
 pattern="AABA"
 length_string=len(string)
